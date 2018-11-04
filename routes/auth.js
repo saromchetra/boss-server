485a15ad4c6fffbaf6382d72637b86dc3b26a415
chetra <chetrasarom@gmail.com>
2018-11-04 13:02:30 +0700

    textSearch v1

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,7 @@ const axios = require('axios');
 var key = "AIzaSyBBKbVpsJQrP1HSN04zi7lX1uhBahZE0FM";
 var apiUrl = "https://www.googleapis.com/youtube/v3/search?&order=date&part=snippet"+'&key='+key;
 var apiUrlPlayList = "https://www.googleapis.com/youtube/v3/playlists?part=id,snippet,contentDetails"+'&key='+key;
+var apiSearchUrl = "https://www.googleapis.com/youtube/v3/search?part=id,snippet"+'&key='+key;
 
 router.get('/getChannel', function(req, res, next) {
     if (req.query.nextPageToken) {
@@ -29,6 +30,18 @@ router.get('/getPlaylist', function(req, res, next) {
     }
 
     axios.get(apiUrlPlayList+'&maxResults='+req.query.maxResults+"&channelId="+req.query.channelId)
+        .then(response => {
+            res.json( response.data );
+        })
+        .catch(error => {
+            res.json( error );
+            console.log(error);
+        });
+
+});
+
+router.get('/searchChannel', function(req, res, next) {
+    axios.get(apiSearchUrl+'&maxResults='+req.query.maxResults+"&channelId="+req.query.channelId+"q="+ req.query.textSearch+"type="+req.query.Type)
         .then(response => {
             res.json( response.data );
         })
