1129259673563f2d893973fcf503f37db0296dd8
chetra <chetrasarom@gmail.com>
2018-11-04 12:36:19 +0700

    update api test4

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,7 @@ const axios = require('axios');
 
 var key = "AIzaSyBBKbVpsJQrP1HSN04zi7lX1uhBahZE0FM";
 var apiUrl = "https://www.googleapis.com/youtube/v3/search?&order=date&part=snippet"+'&key='+key;
+var apiUrlPlayList = "https://www.googleapis.com/youtube/v3/search?part=id,snippet,contentDetails"+'&key='+key;
 
 router.get('/getChannel', function(req, res, next) {
     axios.get(apiUrl+'&maxResults='+req.query.maxResults+"&channelId="+req.query.channelId)
@@ -19,7 +20,7 @@ router.get('/getChannel', function(req, res, next) {
 });
 
 
-router.get('/nextChannelPage', function(req, res, next) {
+router.get('/getNextChannel', function(req, res, next) {
     axios.get(apiUrl+'&maxResults='+req.query.maxResults+"&channelId="+req.query.channelId+"&pageToken="+req.query.nextPageToken)
         .then(response => {
             res.json( response.data );
@@ -30,18 +31,29 @@ router.get('/nextChannelPage', function(req, res, next) {
     });
 });
 
-// /* GET users listing. */
-// router.get('/getChannel', function(req, res, next) {
-//     axios.get("https://www.googleapis.com/youtube/v3/search?part=id,snippet&type=video&order=date&channelId="+ req.query.channelId +"&maxResults=38&key=AIzaSyCDgM8GS68TFVyyE1hE56mDM3Pmsv0995s")
-//         .then(response => {
-//             res.json( response.data );
-//         })
-//         .catch(error => {
-//             res.json( error );
-//             console.log(error);
-//         });
-//
-// });
+router.get('/getPlaylist', function(req, res, next) {
+    axios.get(apiUrlPlayList+'&maxResults='+req.query.maxResults+"&channelId="+req.query.channelId)
+        .then(response => {
+            res.json( response.data );
+        })
+        .catch(error => {
+            res.json( error );
+            console.log(error);
+        });
+
+});
+
+router.get('/getNextPlaylist', function(req, res, next) {
+    axios.get(apiUrlPlayList+'&maxResults='+req.query.maxResults+"&channelId="+req.query.channelId+"&pageToken="+req.query.nextPageToken)
+        .then(response => {
+            res.json( response.data );
+        })
+        .catch(error => {
+            res.json( error );
+            console.log(error);
+        });
+
+});
 
 
 module.exports = router;
