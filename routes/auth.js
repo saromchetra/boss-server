1b530d257e573de92139ae4cccc854f2b90c2713
chetra <chetrasarom@gmail.com>
2018-11-04 11:38:32 +0700

    new api 1

--- routes/auth.js
+++ routes/auth.js
@@ -4,23 +4,11 @@ var router = express.Router();
 const axios = require('axios');
 
 
-// var key = "AIzaSyBBKbVpsJQrP1HSN04zi7lX1uhBahZE0FM";
-// var apiUrl = "https://www.googleapis.com/youtube/v3/search?";
-//
-// router.get('/getChannel', function(req, res) {
-//     axios.get(this.apiUrl+'maxResults='+req.query.maxResults+'&key='+key+"&order=date&part=snippet&channelId="+req.query.channelId)
-//         .then(response => {
-//             res.json( response.data );
-//         })
-//         .catch(error => {
-//             res.json( error );
-//             console.log(error);
-//         });
-// });
+var key = "AIzaSyBBKbVpsJQrP1HSN04zi7lX1uhBahZE0FM";
+var apiUrl = "https://www.googleapis.com/youtube/v3/search?&order=date&part=snippet";
 
-/* GET users listing. */
 router.get('/getChannel', function(req, res, next) {
-    axios.get("https://www.googleapis.com/youtube/v3/search?part=id,snippet&type=video&order=date&channelId="+ req.query.channelId +"&maxResults=38&key=AIzaSyCDgM8GS68TFVyyE1hE56mDM3Pmsv0995s")
+    axios.get(this.apiUrl+'maxResults='+req.query.maxResults+'&key='+key+"&channelId="+req.query.channelId)
         .then(response => {
             res.json( response.data );
         })
@@ -28,8 +16,20 @@ router.get('/getChannel', function(req, res, next) {
             res.json( error );
             console.log(error);
         });
-
 });
+
+// /* GET users listing. */
+// router.get('/getChannel', function(req, res, next) {
+//     axios.get("https://www.googleapis.com/youtube/v3/search?part=id,snippet&type=video&order=date&channelId="+ req.query.channelId +"&maxResults=38&key=AIzaSyCDgM8GS68TFVyyE1hE56mDM3Pmsv0995s")
+//         .then(response => {
+//             res.json( response.data );
+//         })
+//         .catch(error => {
+//             res.json( error );
+//             console.log(error);
+//         });
+//
+// });
 
 
 module.exports = router;
