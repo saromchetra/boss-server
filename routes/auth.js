bc5257e979ea2fabae94dea4b5c217982bf6f577
chetra <chetrasarom@gmail.com>
2018-11-04 12:47:52 +0700

    short api code last v

--- routes/auth.js
+++ routes/auth.js
@@ -23,36 +23,12 @@ router.get('/getChannel', function(req, res, next) {
     });
 });
 
-
-router.get('/getNextChannel', function(req, res, next) {
-    axios.get(apiUrl+'&maxResults='+req.query.maxResults+"&channelId="+req.query.channelId+"&pageToken="+req.query.nextPageToken)
-        .then(response => {
-            res.json( response.data );
-        })
-        .catch(error => {
-            res.json( error );
-            console.log(error);
-    });
-});
-
 router.get('/getPlaylist', function(req, res, next) {
     if (req.query.nextPageToken) {
         apiUrlPlayList = apiUrlPlayList + "&pageToken="+req.query.nextPageToken;
     }
 
     axios.get(apiUrlPlayList+'&maxResults='+req.query.maxResults+"&channelId="+req.query.channelId)
-        .then(response => {
-            res.json( response.data );
-        })
-        .catch(error => {
-            res.json( error );
-            console.log(error);
-        });
-
-});
-
-router.get('/getNextPlaylist', function(req, res, next) {
-    axios.get(apiUrlPlayList+'&maxResults='+req.query.maxResults+"&channelId="+req.query.channelId+"&pageToken="+req.query.nextPageToken)
         .then(response => {
             res.json( response.data );
         })
