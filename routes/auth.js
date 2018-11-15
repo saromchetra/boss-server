13c6700bb924e981b5c4f488d0e2d89b6a2a18bf
chetra <chetrasarom@gmail.com>
2018-11-15 08:07:32 +0700

    short api code last v2

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,7 @@ var apiUrlPlayList = "https://www.googleapis.com/youtube/v3/playlists?part=id,sn
 var apiSearchUrl = "https://www.googleapis.com/youtube/v3/search?part=id,snippet"+'&key='+key;
 
 router.get('/getChannel', function(req, res, next) {
-    if (req.query.nextPageToken) {
+    if (req.query.channelId) {
         apiUrl = apiUrl + "&pageToken="+req.query.pageToken;
     }
 
